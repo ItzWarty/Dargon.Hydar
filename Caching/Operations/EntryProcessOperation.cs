5d946ce4aa7fa20c5316a5d93f4a58abb938e8cf
Michael Yu <ItzWarty@gmail.com>
2015-01-04 02:58:28 -0800

    .

---
+++ Caching/Operations/EntryProcessOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dargon.Hydar.Caching.Operations {
+   public class EntryProcessOperation<K, V, R> : ReturningEntryOperation<K, V, R> {
+      private readonly IEntryProcessor<K, V, R> entryProcessor;
+
+      public EntryProcessOperation(IEntryProcessor<K, V, R> entryProcessor) {
+         this.entryProcessor = entryProcessor;
+      }
+
+      public override EntryOperationAccessFlags AccessFlags { get { return EntryOperationAccessFlags.ReadWrite; } }
+
+      public override R ExecuteInternal(ManageableEntry<K, V> entry) {
+         entryProcessor.Process(entry)
+      }
+   }
+}
